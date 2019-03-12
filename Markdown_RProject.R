cf905058e26188026fd169e4d6318553aa461098
Brock Hayes <bhayes8@murraystate.edu>
2019-03-12 12:39:45 -0500

    Started work on create document-term matrix.

--- Markdown_RProject.R
+++ Markdown_RProject.R
@@ -1,2 +1,13 @@
-rQuestions <- read.csv("Questions.csv", header = TRUE, sep = ",")
-rQuestions
+require('tm')
+library(dplyr)
+install.packages('dplyr')
+##Reading data from csv
+rQuestions <- read.csv("rquestions/Questions.csv", header = TRUE, sep = ",")
+questionTags <- read.csv("rquestions/Tags.csv", header = TRUE, sep = ",")
+
+## Create a data frame
+df1 <-
+## Organize multiple tags in one column
+seperate()
+
+
